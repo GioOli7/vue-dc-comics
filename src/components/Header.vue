f3d0aec06111dc96a839a1be8fbd0d7e179ce475
GioOli7 <gio07live@gmail.com>
2021-05-04 15:44:44 +0200

    header component

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+      header here...
+  </header>
+</template>
+
+<script>
+export default {
+    // per coerenza inserisco lo stesso nome che ha il componente
+    name: 'Header',
+
+}
+</script>
+
+<style>
+
+</style>
